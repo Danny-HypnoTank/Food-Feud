498d888b02f2368601e9d0da3d86b5cb0614a9cb
DominikWaldowski5510 <52669804+DominikWaldowski5510@users.noreply.github.com>
2019-12-10 21:04:33 +0000

    Added all trello stuff between 8 and 9
    Added all trello stuff between 8 and 9

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -3,25 +3,28 @@
  * Name: Alexander Watson
  * Sid: 1507490
  * Date Created: 05/10/2019
- * Last Modified: 06/10/2019
+ * Last Modified: 10/12/2019
  * Modified by: Dominik Waldowski
  */
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     private bool GameIsPaused = false;
     private GameObject pauseMenu;
+    private Loading loading;
 
     private void Awake()
     {
         pauseMenu = GameObject.Find("PausePanel").gameObject;
+        loading = GameObject.Find("LoadingManager").GetComponent<Loading>();
     }
 
     private void Start()
     {
-        Resume();
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
     }
 
     // Update is called once per frame
@@ -45,6 +48,7 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
+        ManageGame.instance.IsTimingDown = true;
     }
 
     private void Pause()
@@ -52,15 +56,12 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        ManageGame.instance.IsTimingDown = false;
     }
 
     public void QuitGame()
     {
-        SceneManager.LoadScene("MainMenu");
-    }
-
-    public void Restart()
-    {
-        SceneManager.LoadScene("GameScene");
+        loading.SetID(0);
+        loading.InitializeLoading();
     }
 }
