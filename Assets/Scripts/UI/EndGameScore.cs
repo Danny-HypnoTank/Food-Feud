0afdf61b650d694cb1690161f95a07b7863659ff
James <Sturdge_666@hotmail.com>
2020-03-24 12:47:27 +0000

    Revert "Antoni hopefully fixed"

--- Assets/Scripts/UI/EndGameScore.cs
+++ Assets/Scripts/UI/EndGameScore.cs
@@ -87,11 +87,6 @@ public class EndGameScore : MonoBehaviour
                 {
                     _currentPodiumPickUp = p.playerNum;
                 }
-
-                if (p.playerNum == medalManager.GetUntouchable())
-                {
-                    _currentPodiumPickUp = p.playerNum;
-                }
             }
 
             medalManager.SpawnMedal(podiumLocations[_currentPodiumStun].transform.position, podiumLocations[_currentPodiumStunOthers].transform.position, 
@@ -139,6 +134,7 @@ public class EndGameScore : MonoBehaviour
                 }
             }
         }
+        medalManager.WriteMedalSaveFile();
     }
 
     //returns to main menu
