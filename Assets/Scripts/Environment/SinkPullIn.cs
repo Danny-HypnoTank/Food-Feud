b394556c30e1b8be94f9aa4373bdc9ab619d9775
James <Sturdge_666@hotmail.com>
2020-03-31 17:04:46 +0100

    Fixed slow down on water pull in

--- Assets/Scripts/Environment/SinkPullIn.cs
+++ Assets/Scripts/Environment/SinkPullIn.cs
@@ -1,54 +1,107 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SinkPullIn : MonoBehaviour
 {
-
+    [Header("Timer Settings")]
     [SerializeField]
     private float duration;
     [SerializeField]
+    private float slowDownStart;
+    [SerializeField]
+    private float pullInStart;
+
+    [Header("Object Settings")]
+    [SerializeField]
     private GameObject waterPlane;
+
+    [Header("Speed Settings")]
     [SerializeField]
     private float baseWaterSpeed;
-    private float waterSpeed;
     [SerializeField]
     private float pullInSpeed;
 
-    private bool isPulling = false;
+    [Header("Modifiers")]
+    [SerializeField, Range(0, 1)]
+    private float speedDownModifier;
 
-    private void OnEnable()
+    private float elapsedTime;
+    private float waterSpeed;
+    private bool isPulling;
+    private bool hasSlowed;
+    private int playerCount;
+
+    private void Awake()
     {
-        StartCoroutine(LateStart());
-        waterSpeed = baseWaterSpeed;
+        playerCount = ManageGame.instance.allPlayerControllers.Count;
+    }
+
+    private void OnDisable()
+    {
+        ResetValues();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Timer();
+
         if (isPulling)
         {
-            for (int i = 0; i < ManageGame.instance.allPlayerControllers.Count; i++)
+            for (int i = 0; i < playerCount; i++)
             {
-                Vector3 forceDirection = this.transform.position - ManageGame.instance.allPlayerControllers[i].transform.position;
+                Vector3 forceDirection = transform.position - ManageGame.instance.allPlayerControllers[i].transform.position;
                 ManageGame.instance.allPlayerControllers[i].chc.Move(forceDirection * pullInSpeed);
             }
         }
         waterPlane.transform.Translate(Vector3.up * waterSpeed * Time.deltaTime);
     }
 
-    IEnumerator LateStart()
+    private void Timer()
     {
-        yield return new WaitForSeconds(duration);
-        isPulling = true;
-        StartCoroutine(ResetTimer());
+        if (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime >= slowDownStart && !hasSlowed)
+            {
+                hasSlowed = true;
+                ModifySpeed(speedDownModifier);
+            }
+
+            if (elapsedTime >= pullInStart && !isPulling)
+            {
+                isPulling = true;
+                waterSpeed = -baseWaterSpeed;
+            }
+
+            CheckTimerEnd();
+        }
     }
 
-    IEnumerator ResetTimer()
+    private void CheckTimerEnd()
     {
-        waterSpeed = -baseWaterSpeed;
-        yield return new WaitForSeconds(duration);
-        gameObject.SetActive(false);
+        if (elapsedTime >= duration)
+        {
+            ModifySpeed();
+
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void ResetValues()
+    {
+        elapsedTime = 0;
+        waterSpeed = baseWaterSpeed;
         isPulling = false;
+        hasSlowed = false;
+    }
+
+    private void ModifySpeed(float multiplier = 1)
+    {
+        for (int i = 0; i < playerCount; i++)
+        {
+            PlayerController currentPlayer = ManageGame.instance.allPlayerControllers[i];
+            currentPlayer.SetProperty<float>(nameof(currentPlayer.MoveSpeedModifier), currentPlayer.BaseSpeed * multiplier);
+        }
     }
 }
