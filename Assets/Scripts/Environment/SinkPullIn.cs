d2ad5be95e62e3171fcffe9f42aa4304220381a4
AntoniGoodday <32736437+AntoniGoodday@users.noreply.github.com>
2020-03-31 02:03:07 +0100

    paint fixes and base implementation of the sink pull in

---
+++ Assets/Scripts/Environment/SinkPullIn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkPullIn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] playerObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LateStart());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < playerObjects.Length; i++)
+        {
+            PlayerController playerToCheck = playerObjects[i].GetComponent<PlayerController>();
+            Vector3 forceDirection = this.transform.position - playerToCheck.transform.position;
+            playerToCheck.GetComponent<CharacterController>().Move(forceDirection * 0.002f);
+                
+            
+        }
+    }
+
+    IEnumerator LateStart()
+    {
+        yield return new WaitForSeconds(2);
+        playerObjects = GameObject.FindGameObjectsWithTag("Player");
+    }
+}
