92fb842bde63205d468c5cc2f94180edafcfec98
Danny-HypnoTank <d.pymhember@aol.co.uk>
2020-04-12 15:22:59 +0100

    Fix podium scoring (needs further testing) and cleaned up useless code in score scene
    - fixed sushi idle to be looping

--- Assets/Scripts/UI/EndGameScore.cs
+++ Assets/Scripts/UI/EndGameScore.cs
@@ -25,10 +25,6 @@ public class EndGameScore : MonoBehaviour
     [SerializeField]
     private Transform[] podiumLocations;
     [SerializeField]
-    private GameObject[] menuButtons;
-    [SerializeField]
-    private GameObject[] winningPlayer;
-    [SerializeField]
     private GameObject[] winnerIcons;
 
     private int usedPodiums;
@@ -38,189 +34,168 @@ public class EndGameScore : MonoBehaviour
 
     MedalManager medalManager;
 
+    //Reseting 
+    private void Awake()
+    {
+        players[0].hasWon = false;
+        players[1].hasWon = false;
+        players[2].hasWon = false;
+        players[3].hasWon = false;
+        Debug.Log("Reset has won");
+        sortedPlayers.Clear();
+    }
     private void Start()
     {
+
         medalManager = MedalManager.Instance;
-
         canUseInput = false;
-        StartCoroutine(WinnerCheck());
-
-        //enables end score menu/rematch buttons
-        foreach (GameObject goMB in menuButtons)
-        {
-            goMB.SetActive(true);
-        }
-        //enables player score texts
-        foreach (GameObject goWP in winningPlayer)
-        {
-            goWP.SetActive(true);
-        }
-
 
         loading = GameObject.Find("LoadingManager").GetComponent<Loading>();
         sortedPlayers = players.OrderByDescending(o => o.playerScore).ToList();
+        //sortedPlayers = players.OrderByDescending(o => o.scorePercentage).ToList();
+
+        #region stunMedal
+        int _currentPlayer = 0;
+
+        int[] _medalChosen = { 0, 0, 0, 0, 0 };
+
+        bool[] _medalClaimed = { false, false, false, false, false };
+
+        //int[] _medalsPerPlayer = { 0, 0, 0, 0 };
+
+        Dictionary<int, int> _medalsPerPlayer = new Dictionary<int, int>();
+
+        Dictionary<int, int> _medalForPlayer = new Dictionary<int, int>();
+
+        foreach (Player p in sortedPlayers)
+        {
+            int _medalAmount = 0;
+
+            if (p.playerNum == 0)
+            {
+                _medalAmount++;
+            }
+
+            if (p.playerNum == medalManager.GetTopStunned())
+            {
+                _medalAmount++;
+            }
+
+            if (p.playerNum == medalManager.GetTopStunOther())
+            {
+                _medalAmount++;
+            }
+
+            if (p.playerNum == medalManager.GetTopDashes())
+            {
+
+                _medalAmount++;
+            }
+
+            if (p.playerNum == medalManager.GetTopPowerPickup())
+            {
+                _medalAmount++;
+            }
+
+            if (p.playerNum == medalManager.GetUntouchable())
+            {
+                _medalAmount++;
+            }
+
+            _medalsPerPlayer.Add(p.playerNum, _medalAmount);
+        }
+
+        var _medalList = from pair in _medalsPerPlayer
+                         orderby pair.Value ascending
+                         select pair;
+
+
+        foreach (KeyValuePair<int, int> pair in _medalList)
         {
 
-            #region stunMedal
-            
 
-            int _currentPlayer = 0;
-
-            int[] _medalChosen = { 0, 0, 0, 0, 0 };
-
-            bool[] _medalClaimed = { false, false, false, false, false };
-
-            //int[] _medalsPerPlayer = { 0, 0, 0, 0 };
-
-            Dictionary<int, int> _medalsPerPlayer = new Dictionary<int, int>();
-
-            Dictionary<int, int> _medalForPlayer = new Dictionary<int, int>();
-
-
-
-            foreach (Player p in sortedPlayers)
+            if (pair.Key == medalManager.GetTopStunned() && _medalClaimed[0] == false)
             {
-                int _medalAmount = 0;
-
-                if(p.playerNum == 1)
+                if (pair.Key != 0)
                 {
-                    _medalAmount++;
-                }
-
-                if (p.playerNum == medalManager.GetTopStunned())
-                {
-                    _medalAmount++;
-                }
-
-                if (p.playerNum == medalManager.GetTopStunOther())
-                {
-                    _medalAmount++;
-                }
-
-                if (p.playerNum == medalManager.GetTopDashes())
-                {
-
-                    _medalAmount++;
-                }
-
-                if (p.playerNum == medalManager.GetTopPowerPickup())
-                {
-                    _medalAmount++;
-                }
-
-                if (p.playerNum == medalManager.GetUntouchable())
-                {
-                    _medalAmount++;
-                }
-
-                _medalsPerPlayer.Add(p.playerNum, _medalAmount);
-
-                
-
-
-            }
-
-            var _medalList = from pair in _medalsPerPlayer
-                             orderby pair.Value ascending
-                             select pair;
-
-
-            foreach(KeyValuePair<int,int> pair in _medalList)
-            {
-                
-
-                if (pair.Key == medalManager.GetTopStunned() && _medalClaimed[0] == false)
-                {
-                    if (pair.Key != 0)
-                    {
-                        _medalClaimed[0] = true;
-                        _medalForPlayer.Add(pair.Key, 0);
-                    }
-                }
-                else if (pair.Key == medalManager.GetTopStunOther() && _medalClaimed[1] == false)
-                {
-                    if (pair.Key != 0)
-                    {
-                        _medalClaimed[1] = true;
-                        _medalForPlayer.Add(pair.Key, 1);
-                    }
-                }
-                else if (pair.Key == medalManager.GetTopDashes() && _medalClaimed[2] == false)
-                {
-                    if (pair.Key != 0)
-                    {
-                        _medalClaimed[2] = true;
-                        _medalForPlayer.Add(pair.Key, 2);
-                    }
-                }
-                else if (pair.Key == medalManager.GetTopPowerPickup() && _medalClaimed[3] == false)
-                {
-                    if (pair.Key != 0)
-                    {
-                        _medalClaimed[3] = true;
-                        _medalForPlayer.Add(pair.Key, 3);
-                    }
-                }
-                else if (pair.Key == medalManager.GetUntouchable() && _medalClaimed[4] == false)
-                {
-                   
-                        _medalClaimed[4] = true;
-                        _medalForPlayer.Add(pair.Key, 4);
-                    
+                    _medalClaimed[0] = true;
+                    _medalForPlayer.Add(pair.Key, 0);
                 }
             }
-
-
-            foreach(KeyValuePair<int,int> pair in _medalForPlayer)
+            else if (pair.Key == medalManager.GetTopStunOther() && _medalClaimed[1] == false)
             {
-                medalManager.SpawnMedal(podiumLocations[pair.Key].transform.position, pair.Value);
-            }
-            
-
-            
-
-
-            #endregion
-
-            loading = GameObject.Find("LoadingManager").GetComponent<Loading>();
-            sortedPlayers = players.OrderByDescending(o => o.scorePercentage).ToList();
-
-            for (int i = 0; i < sortedPlayers.Count; i++)
-            {
-                sortedPlayers[i].scorePercentage = 0;
-                if (i == 0)
+                if (pair.Key != 0)
                 {
-                    sortedPlayers[i].hasWon = true;
-                }
-                else
-                {
-                    sortedPlayers[i].hasWon = false;
+                    _medalClaimed[1] = true;
+                    _medalForPlayer.Add(pair.Key, 1);
                 }
             }
-            foreach (Player p in players)
+            else if (pair.Key == medalManager.GetTopDashes() && _medalClaimed[2] == false)
             {
-
-                total += p.playerScore;
-
-            }
-            for (int i = 0; i < thePlayerData.Length; i++)
-            {
-                thePlayerData[i].gameObject.SetActive(false);
-            }
-            usedPodiums = 0;
-            for (int i = 0; i < players.Length; i++)
-            {
-                if (players[i].isActivated == true && players[i].isLocked == true)
+                if (pair.Key != 0)
                 {
-                    thePlayerData[usedPodiums].GetComponent<PodiumController>().AddPlayer(players[i]);
-                    thePlayerData[usedPodiums].transform.position = podiumLocations[usedPodiums].transform.position;
-                    thePlayerData[usedPodiums].gameObject.SetActive(true);
-                    thePlayerData[usedPodiums].GetComponent<PodiumController>().SetTotal(total);
-                    usedPodiums++;
-                    //SoundManager.Instance.SetBGM("Win");
+                    _medalClaimed[2] = true;
+                    _medalForPlayer.Add(pair.Key, 2);
                 }
+            }
+            else if (pair.Key == medalManager.GetTopPowerPickup() && _medalClaimed[3] == false)
+            {
+                if (pair.Key != 0)
+                {
+                    _medalClaimed[3] = true;
+                    _medalForPlayer.Add(pair.Key, 3);
+                }
+            }
+            else if (pair.Key == medalManager.GetUntouchable() && _medalClaimed[4] == false)
+            {
+
+                _medalClaimed[4] = true;
+                _medalForPlayer.Add(pair.Key, 4);
+
             }
         }
+
+        foreach (KeyValuePair<int, int> pair in _medalForPlayer)
+        {
+            medalManager.SpawnMedal(podiumLocations[pair.Key].transform.position, pair.Value);
+        }
+        #endregion
+
+        for (int i = 0; i < sortedPlayers.Count; i++)
+        {
+            if (i == 0)
+            {
+                sortedPlayers[i].hasWon = true;
+            }
+        }
+        //Setting total to players score to then scale podium by their individual scores
+        foreach (Player p in players)
+        {
+
+            total += p.playerScore;
+
+        }
+
+        for (int i = 0; i < thePlayerData.Length; i++)
+        {
+            thePlayerData[i].gameObject.SetActive(false);
+        }
+
+        usedPodiums = 0;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].isActivated == true && players[i].isLocked == true)
+            {
+                thePlayerData[usedPodiums].GetComponent<PodiumController>().AddPlayer(players[i]);
+                thePlayerData[usedPodiums].transform.position = podiumLocations[usedPodiums].transform.position;
+                thePlayerData[usedPodiums].gameObject.SetActive(true);
+                thePlayerData[usedPodiums].GetComponent<PodiumController>().SetTotal(total);
+                usedPodiums++;
+                //SoundManager.Instance.SetBGM("Win");
+            }
+        }
+        StartCoroutine(WinnerCheck());
         medalManager.WriteMedalSaveFile();
     }
 
@@ -229,17 +204,7 @@ public class EndGameScore : MonoBehaviour
     {
         if (canUseInput)
         {
-            loading.InitializeLoading();
-
-            //disables player score texts and menu buttons when main menu button is pressed
-            foreach (GameObject goMB in menuButtons)
-            {
-                goMB.SetActive(false);
-            }
-            foreach (GameObject goWP in winningPlayer)
-            {
-                goWP.SetActive(false);
-            }
+            sortedPlayers.Clear();
 
             foreach (Player p in players)
             {
@@ -248,9 +213,10 @@ public class EndGameScore : MonoBehaviour
                 p.isLocked = false;
                 p.isActivated = false;
                 p.skinId = 0;
-
+                p.scorePercentage = 0;
             }
 
+            loading.InitializeLoading();
             SceneManager.LoadScene(0);
         }
     }
@@ -260,27 +226,17 @@ public class EndGameScore : MonoBehaviour
     {
         if (canUseInput)
         {
-            loading.InitializeLoading();
+            sortedPlayers.Clear();
 
-            //disables player score texts and menu buttons when rematch button is pressed
-            foreach (GameObject goMB in menuButtons)
-            {
-                goMB.SetActive(false);
-            }
-            foreach (GameObject goWP in winningPlayer)
-            {
-                goWP.SetActive(false);
-            }
 
-            foreach (Player p in sortedPlayers)
+            foreach (Player p in players)
             {
 
                 p.hasWon = false;
-
+                p.scorePercentage = 0;
             }
 
-            //reload characters
-
+            loading.InitializeLoading();
             SceneManager.LoadScene(1);
         }
     }
