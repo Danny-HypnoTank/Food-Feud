670921634d6f845d82cc8c9e933643d29bf3bd0a
James <Sturdge_666@hotmail.com>
2020-04-20 00:41:06 +0100

    Rematch/MainMenu buttons now move like the main menu buttons

---
+++ Assets/Scripts/UI/UIElementControllerImage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIElementControllerImage : UIElementController
+{
+    [Header("Graphics")]
+    [SerializeField]
+    private Sprite inactiveSprite;
+    [SerializeField]
+    private Sprite activeSprite;
+
+    private Image spriteRender;
+
+    void Start()
+    {
+        spriteRender = GetComponent<Image>();
+    }
+
+    private void ChangeSprite(Sprite sprite)
+    {
+        if (spriteRender != null)
+        {
+            if (spriteRender.sprite != sprite)
+                spriteRender.sprite = sprite;
+        }
+    }
+
+    public override void SetHover()
+    {
+        ChangeSprite(activeSprite);
+        base.SetHover();
+    }
+
+    public override void SetInactive()
+    {
+        ChangeSprite(inactiveSprite);
+        base.SetInactive();
+    }
+}
---
+++ Assets/Scripts/UI/UIElementControllerSprite.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIElementControllerSprite : UIElementController
+{
+    [Header("Graphics")]
+    [SerializeField]
+    private Sprite inactiveSprite;
+    [SerializeField]
+    private Sprite activeSprite;
+
+    private SpriteRenderer spriteRender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void ChangeSprite(Sprite sprite)
+    {
+        if (spriteRender != null)
+        {
+            if (spriteRender.sprite != sprite)
+                spriteRender.sprite = sprite;
+        }
+    }
+
+    public override void SetHover()
+    {
+        base.SetHover();
+
+        ChangeSprite(activeSprite);
+    }
+
+    public override void SetInactive()
+    {
+        base.SetInactive();
+
+        ChangeSprite(inactiveSprite);
+    }
+}
